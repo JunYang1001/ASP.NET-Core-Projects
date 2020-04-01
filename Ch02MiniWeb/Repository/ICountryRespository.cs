bfa8c2f5a1de4fb7a30498a3ab09b27114b296f5
Junyang Zhang <zhangjunyang94@gmail.com>
2020-04-02 01:43:52 +0800

    ASP.NET Core开发实战CH02

---
+++ Ch02MiniWeb/Repository/ICountryRespository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ch02MiniWeb.Model;
+
+namespace Ch02MiniWeb.Repository
+{
+    interface ICountryRespository
+    {
+        IQueryable<Country> All();
+        Country Find(string code);
+        IQueryable<Country> AllBy(string filter);
+    }
+}
---
+++ Ch02MiniWeb/Startup.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ch02MiniWeb.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+
+namespace Ch02MiniWeb
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ICountryRespository,CountryRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.Map("/country", countryApp =>
+            {
+                countryApp.Run(async (context) =>
+                {
+                   // var country = provider.GetRequiredService<ICountryRespository>();
+                    var query = context.Request.Query["q"];
+                    var list = (app.ApplicationServices).GetService<ICountryRespository>().AllBy(query).ToList();
+                    var json = JsonConvert.SerializeObject(list);
+
+                    await context.Response.WriteAsync(json);
+                });
+            });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    //var country = provider.GetService<ICountryRespository>();
+                    //var query=context.Request.Query["q"];
+                    //var countries = country.AllBy(query).ToList();
+                    //var serializeObjectjson = JsonConvert.SerializeObject(countries);
+                    await context.Response.WriteAsync("Invalid call");
+                });
+            });
+        }
+    }
+}
